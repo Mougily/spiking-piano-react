aeca6549182d27781b9e2ce660de994f97b9e2c2
isamotta <isabelatlmotta@gmail.com>
2023-02-17 07:32:40 -0300

    using react-piano

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Oscillator from "./Oscillator";
+import ChangeWaveform from "./ChangeWaveform";
+import Gain from "./Gain";
+import Filter from "./Filter";
 
-function App() {
+const App = () => {
+  const [effects, setEffects] = useState({
+    waveform: "sine",
+    gainValue: 0.15,
+    filterType: "lowpass",
+    filterFreq: 15000
+  });
+
+  const inputChange = e => {
+    setEffects({ ...effects, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="piano">
+        <Oscillator
+          waveform={effects.waveform}
+          filterType={effects.filterType}
+          filterFreq={effects.filterFreq}
+          gainValue={effects.gainValue}
+        />
+      </div>
+      <div className="effects">
+        <ChangeWaveform inputChange={inputChange} waveform={effects.waveform} />
+        <Gain inputChange={inputChange} gainValue={effects.gainValue} />
+        <Filter
+          inputChange={inputChange}
+          filterType={effects.filterType}
+          filterFreq={effects.filterFreq}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
